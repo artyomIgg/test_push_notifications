0e94cf97ed4d855b4ab84a08646a25d8606005e9
artyomIgg <hvalven1@gmail.com>
2022-01-28 22:47:22 +0200

    Add green world design

--- lib/earth/earth.dart
+++ lib/earth/earth.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_cube/flutter_cube.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:flutter_svg/parser.dart';
 import 'dart:math' as math;
 import 'dart:ui' as ui;
 // import 'package:flutter_earth/flutter_earth.dart';
@@ -21,13 +23,25 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> with TickerProviderStateMixin {
   late AnimationController _controller;
   late AnimationController _earthSizeAnimationController;
+  late AnimationController _earthDoubleTapSizeController;
   late Object _earth;
   late Scene _scene;
   late double _earthRotationY;
   late Vector2 startMove;
   late Vector2 updateMove;
   late bool isControllerStop = false;
+  Vector3 defaultEarthSize = Vector3(9, 9, 9);
+  Vector3 scaleMinEarthSize = Vector3(5, 5, 5);
+  Vector3 scaleMaxEarthSize = Vector3(12, 12, 12);
+  Color backgroundColor = Color(0xff191C1A);
+  Color cardGradientFirstGreen = Color(0xff134E5E);
+  Color cardGradientSecondGreen = Color(0xff71B280);
+  Color cardGradientFirstBlue = Color(0xff4B6CB7);
+  Color cardGradientSecondBlue = Color(0xff182848);
+  Color textCountColorGreen = Color(0xff88F9BA);
+  Color textCountColorBlue = Color(0xffBFE9F8);
   bool isTap = false;
+  // bool isDoubleTap = false;
 
   void generateSphereObject(Object parent, String name, double radius,
       bool backfaceCulling, String texturePath) async {
@@ -41,7 +55,7 @@ class _MyAppState extends State<MyApp> with TickerProviderStateMixin {
   @override
   void initState() {
     super.initState();
-    _earth = Object(name: 'earth', scale: Vector3(5.0, 5.0, 5.0));
+    _earth = Object(name: 'earth', scale: defaultEarthSize);
     generateSphereObject(
         _earth, 'surface', 0.485, true, 'res/3d_model/flutter8.png');
     _controller = AnimationController(
@@ -61,16 +75,33 @@ class _MyAppState extends State<MyApp> with TickerProviderStateMixin {
       vsync: this,
     )..addListener(() {
         if (_earth != null) {
-          _earth.scale.xyz = Vector3(
-              _earthSizeAnimationController.value * 5,
-              _earthSizeAnimationController.value * 5,
-              _earthSizeAnimationController.value * 5);
+          _earth.scale.setFrom(
+              defaultEarthSize.xyz * _earthSizeAnimationController.value);
           _earth.updateTransform();
           _scene.update();
         }
       });
 
-
+    _earthDoubleTapSizeController = AnimationController(
+      duration: Duration(milliseconds: 800),
+      vsync: this,
+    )..addListener(() {
+        if (_earth != null &&
+            (_earthDoubleTapSizeController.value * scaleMaxEarthSize.x >=
+                defaultEarthSize.x)) {
+          _earth.scale.setFrom(
+              scaleMaxEarthSize.xyz * _earthDoubleTapSizeController.value);
+          _earth.updateTransform();
+          _scene.update();
+        }
+      });
+    // ..addStatusListener((status) {
+    //   if (status == AnimationStatus.completed && isDoubleTap) {
+    //     Future.delayed(Duration(milliseconds: 3000)).then((value) {
+    //       _earthDoubleTapSizeController.reverse();
+    //     });
+    //   }
+    // });
 
     _earthRotationY = _controller.value * 360;
   }
@@ -94,90 +125,216 @@ class _MyAppState extends State<MyApp> with TickerProviderStateMixin {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: GestureDetector(
-            onTap: () {
-              setState(() {
-                _controller.stop();
-                if (!isTap) {
-                  _earthSizeAnimationController.reverse();
-                } else {
-                  _earthSizeAnimationController.forward();
-                }
-                isTap = !isTap;
-                // _scene.world.visiable = !_scene.world.visiable;
-                // _earthSizeAnimationController.reverse();
-              });
-            },
-
-            onPanCancel: () {},
-
-            onPanDown: (_) {},
-
-            onPanEnd: (_) {},
-
-            onPanStart: (_) {},
-
-            onPanUpdate: (_) {},
-
-            onDoubleTap: () {
-              _controller.stop();
-            },
-
-            // onScaleUpdate: (_) {
-            //
-            // },
-            child: Listener(
-                onPointerDown: (_) {
-                  setState(() {
-                    _controller.stop();
-                  });
-                },
-                onPointerMove: (_) {
-                  setState(() {
-                    _controller.stop();
-                  });
-                },
-                onPointerUp: (_) {
-                  Future.delayed(Duration(milliseconds: 2000)).then((value) {
-                    _controller
-                      ..forward()
-                      ..repeat();
-                  });
-                },
-                child: Stack(
+      home: SafeArea(
+        child: Scaffold(
+          body: Container(
+            height: double.infinity,
+            width: double.infinity,
+            color: backgroundColor,
+            child: Center(
+              child: SingleChildScrollView(
+                child: Column(
                   children: [
-                    Cube(onSceneCreated: _onSceneCreated),
-                    AnimatedSwitcher(
-                      duration: Duration(milliseconds: 600),
-                      transitionBuilder:
-                          (Widget child, Animation<double> animation) {
-                        return ScaleTransition(scale: animation, child: child);
-                      },
-                      child: isTap
-                          ? ClipRRect(
-                        borderRadius: BorderRadius.circular(0),
-                            // clipper: CustomClipper,
-                          // decoration:  BoxDecoration(
-                          //     shape: BoxShape.circle,
-                          //     image: DecorationImage(
-                          //         fit: BoxFit.fill,
-                          //         image:  Image.asset("res/3d_model/flutter8.png")
-                          //     )
-                          // ),
-                              child: PhotoView(
-                                initialScale: 0.35,
-                                backgroundDecoration:
-                                    BoxDecoration(color: Colors.transparent),
-                                imageProvider:
-                                    AssetImage("res/3d_model/flutter8.png"),
-                              ),
-                            )
-                          : const SizedBox(),
+                    Padding(
+                      padding: const EdgeInsets.all(16),
+                      child: Row(
+                        mainAxisSize: MainAxisSize.max,
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: const [
+                          Text(
+                            "Current Situation",
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 28,
+                                fontWeight: FontWeight.w700),
+                          ),
+                          Icon(
+                            Icons.wb_sunny,
+                            color: Colors.orange,
+                          )
+                        ],
+                      ),
                     ),
+                    Container(
+                      height: 327,
+                      padding: EdgeInsets.symmetric(horizontal: 16),
+                      child: GestureDetector(
+                        onTap: () {
+                          setState(() {
+                            _controller.stop();
+                            if (!isTap) {
+                              _earthSizeAnimationController.reverse();
+                            } else {
+                              _earthSizeAnimationController.forward();
+                            }
+                            isTap = !isTap;
+                          });
+                        },
+
+                        onPanCancel: () {},
+
+                        onPanDown: (_) {},
+
+                        onPanEnd: (_) {},
+
+                        onPanStart: (_) {},
+
+                        onPanUpdate: (_) {},
+
+                        onLongPress: () {
+                          _controller.stop();
+                          _earthDoubleTapSizeController.forward();
+                        },
+
+                        onLongPressUp: () {
+                          _controller.forward();
+                          _earthDoubleTapSizeController.reverse();
+                        },
+
+                        // onLong
+
+                        // onDoubleTap: () {
+                        //   !isDoubleTap
+                        //       ? ({
+                        //           _controller.stop(),
+                        //           _earthDoubleTapSizeController.reverse(),
+                        //           isDoubleTap = !isDoubleTap,
+                        //         })
+                        //       : ({
+                        //       _earthDoubleTapSizeController.forward(), isDoubleTap = !isDoubleTap
+                        //   });
+                        //   // _earthDoubleTapSizeController.forward();
+                        //   // _earthSizeAnimationController.forward();
+                        // },
+
+                        // onScaleUpdate: (_) {
+                        //
+                        // },
+                        child: Listener(
+                            onPointerHover: (_) {
+                              print("onPointerHover");
+                            },
+                            onPointerDown: (_) {
+                              setState(() {
+                                _controller.stop();
+                              });
+                            },
+                            onPointerMove: (_) {
+                              setState(() {
+                                _controller.stop();
+                              });
+                            },
+                            onPointerUp: (_) {
+                              Future.delayed(Duration(milliseconds: 2000))
+                                  .then((value) {
+                                _controller
+                                  ..forward()
+                                  ..repeat();
+                              });
+                            },
+                            child: Stack(
+                              children: [
+                                ClipRRect(
+                                    child:
+                                        Cube(onSceneCreated: _onSceneCreated)),
+                                AnimatedSwitcher(
+                                  duration: Duration(milliseconds: 600),
+                                  transitionBuilder: (Widget child,
+                                      Animation<double> animation) {
+                                    return ScaleTransition(
+                                        scale: animation, child: child);
+                                  },
+                                  child: isTap
+                                      ? ClipRRect(
+                                          borderRadius:
+                                              BorderRadius.circular(0),
+                                          // clipper: CustomClipper,
+                                          // decoration:  BoxDecoration(
+                                          //     shape: BoxShape.circle,
+                                          //     image: DecorationImage(
+                                          //         fit: BoxFit.fill,
+                                          //         image:  Image.asset("res/3d_model/flutter8.png")
+                                          //     )
+                                          // ),
+                                          child: PhotoView(
+                                            initialScale: 0.35,
+                                            backgroundDecoration: BoxDecoration(
+                                                color: Colors.transparent),
+                                            imageProvider: AssetImage(
+                                                "res/3d_model/flutter8.png"),
+                                          ),
+                                        )
+                                      : const SizedBox(),
+                                ),
+                              ],
+                            )),
+                      ),
+                    ),
+                    Padding(
+                        padding: EdgeInsets.all(16),
+                        child: Row(
+                          children: [
+                            Expanded(
+                              child: smallCardInfo(
+                                  cardGradientFirstGreen,
+                                  cardGradientSecondGreen,
+                                  "Plants",
+                                  "256k",
+                                  "+4% of target",
+                                "res/bihance_pic/graph_green.svg",
+                                textCountColorGreen
+                              ),
+                            ),
+                            SizedBox(
+                              width: 16,
+                            ),
+                            Expanded(
+                                child: smallCardInfo(
+                              cardGradientFirstBlue,
+                              cardGradientSecondBlue,
+                              "Users",
+                              "128k",
+                              "+22% of target",
+                                  "res/bihance_pic/graph_blue.svg",
+                                  textCountColorBlue
+                            )),
+                            Container(),
+                          ],
+                        )),
+                    Padding(
+                        padding: EdgeInsets.all(16),
+                        child: Row(
+                          children: [
+                            Container(
+                              color: Colors.white,
+                              height: 190,
+                              width: 200,
+                            ),
+                            SizedBox(
+                              width: 16,
+                            ),
+                            Container(),
+                          ],
+                        )),
+                    Padding(
+                        padding: EdgeInsets.all(16),
+                        child: Row(
+                          children: [
+                            Container(
+                              color: Colors.white,
+                              height: 190,
+                              width: 200,
+                            ),
+                            SizedBox(
+                              width: 16,
+                            ),
+                            Container(),
+                          ],
+                        ))
                   ],
-                )),
+                ),
+              ),
+            ),
           ),
         ),
       ),
@@ -185,6 +342,54 @@ class _MyAppState extends State<MyApp> with TickerProviderStateMixin {
   }
 }
 
+Widget smallCardInfo(Color firstColor, Color secondColor, String title,
+    String count, String target, String path, Color textColor) {
+  return Container(
+    decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.all(Radius.circular(16)),
+        gradient: LinearGradient(colors: [
+          firstColor,
+          secondColor,
+        ])),
+    height: 190,
+    width: 200,
+    child: Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Column(
+        mainAxisSize: MainAxisSize.max,
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          Row(
+            children: [
+              Text(title, style: TextStyle(fontSize: 18, color: Colors.white),),
+            ],
+          ),
+          SizedBox(height: 14,),
+          Row(
+            children: [
+              Text(count, style: TextStyle(fontSize: 34, color: Colors.white, fontWeight: FontWeight.w700),),
+            ],
+          ),
+          Row(
+            children: [
+              Text(target, style: TextStyle(fontSize: 16, color: textColor),),
+            ],
+          ),
+          Expanded(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              mainAxisSize: MainAxisSize.max,
+              children: [
+                SvgPicture.asset(path),
+              ],
+            ),
+          ),
+        ],
+      ),
+    ),
+  );
+}
 
 Future<Mesh> generateSphereMesh(
     {num radius = 0.5,
